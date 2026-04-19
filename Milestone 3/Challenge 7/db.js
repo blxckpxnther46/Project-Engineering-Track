0897fa79b342806bcee10fb0dfe4872c345a3683
blxckpxnther <parthasarathy5117r@gmail.com>
2026-04-19 22:12:24 +0530

    fix: db connection for render

--- Milestone 3/Challenge 7/db.js
+++ Milestone 3/Challenge 7/db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
