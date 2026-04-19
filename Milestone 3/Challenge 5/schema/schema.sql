136816d1467751fa0f6af31f0c3e9cb09a442eda
blxckpxnther <parthasarathy5117r@gmail.com>
2026-04-19 20:22:26 +0530

    Added challenge 7 + refactored schema.sql on challenge 5

--- Milestone 3/Challenge 5/schema/schema.sql
+++ Milestone 3/Challenge 5/schema/schema.sql
@@ -1,17 +1,20 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
+  id INT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE projects (
-    id INT PRIMARY KEY,
-    project_name VARCHAR(100)
+  id INT PRIMARY KEY,
+  project_name VARCHAR(100) NOT NULL,
+  owner_id INT,
+  FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE tasks (
-    id INT PRIMARY KEY,
-    title VARCHAR(255),
-    priority INT,
-    project_id INT
+  id INT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  priority INT CHECK (priority BETWEEN 1 AND 5),
+  project_id INT,
+  FOREIGN KEY (project_id) REFERENCES projects(id)
 );
