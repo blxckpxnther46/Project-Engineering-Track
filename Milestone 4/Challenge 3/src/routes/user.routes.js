1cb2d8a0b5c6aaf2565d9b960e4cd12208dc3486
blxckpxnther <parthasarathy5117r@gmail.com>
2026-04-24 22:48:47 +0530

    fix/error-handler

--- Milestone 4/Challenge 3/src/routes/user.routes.js
+++ Milestone 4/Challenge 3/src/routes/user.routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUser, deleteUser, crashTest } = require('../user.controller');
+const controller = require('../controllers/user.controller');
 
-router.post('/', createUser);
-router.get('/crash/test', crashTest);
-router.get('/:id', getUser);
-router.delete('/:id', deleteUser);
+router.get('/:id', controller.getUser);
+router.get('/crash', controller.crashTest);
 
 module.exports = router;
